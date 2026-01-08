816af877a1d04a8f66acd9a91d3f639dfc3a2d03
000hcl <mnrjohansson@gmail.com>
2026-01-08 16:50:01 +0200

    test render actions push

--- dockertest/src/App.jsx
+++ dockertest/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
       <button onClick={()=>setCount(count+1)}>add</button>
       <button onClick={()=>setCount(count-1)}>subtract</button>
       <button onClick={()=>setCount(0)}>reset to 0</button>
+
+      <h2>version 2</h2>
     </>
   )
 }
